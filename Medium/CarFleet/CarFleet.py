3de0395d5a61d32c89a43a2c252a1cacd82a94d7
3zden <azeddineabouaam1@gmail.com>
2025-09-27 15:38:52 +0100

    CarFleet Problem

---
+++ Medium/CarFleet/CarFleet.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def carFleet(self, target, position, speed):
+        """
+        :type target: int
+        :type position: List[int]
+        :type speed: List[int]
+        :rtype: int
+        """
+        # duration = position / speed 
+        duration = [po / sp for po in position for sp in speed ]
+        return duration
