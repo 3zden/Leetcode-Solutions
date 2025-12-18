8bcaf4004a1cb3e6773ef523a3c1b23458f8cbc1
3zden <azeddineabouaam1@gmail.com>
2025-12-18 14:37:49 +0100

    solve 485. Max Consecutive Ones

---
+++ Easy/485. Max Consecutive Ones/solution.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def findMaxConsecutiveOnes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        maxx = 0
+        streak = 0
+        for v in nums:
+            if v == 1:
+                streak += 1
+            else : 
+                maxx = max(maxx,streak)
+                streak = 0
+        maxx = max(streak,maxx)
+        return maxx
