4e911d648283fbca2c5c3fbd931294e9b0b88f2f
3zden <azeddineabouaam1@gmail.com>
2025-12-29 01:40:26 +0100

    solve 860. Lemonade Change

---
+++ Easy/860. Lemonade Change/solution.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def lemonadeChange(self, bills):
+        """
+        :type bills: List[int]
+        :rtype: bool
+        """
+        f = 0
+        t = 0
+        for i in bills:
+            if i == 5 :
+                f += 1
+            elif i == 10:
+                if f > 0 :
+                    f -= 1
+                else : return False
+                t += 1
+            else :
+                if (f > 0 and t > 0) :
+                    f -= 1
+                    t -= 1
+                elif f > 2 :
+                    f -= 3
+                else : return False
+        return True
+        
