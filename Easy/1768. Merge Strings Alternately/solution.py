be2610e871fd04af7b0df6cd297736beedd00fc7
3zden <azeddineabouaam1@gmail.com>
2026-01-02 23:06:16 +0100

    solve 1768. Merge Strings Alternately

---
+++ Easy/1768. Merge Strings Alternately/solution.py
@@ -0,0 +1,22 @@
+
+class Solution(object):
+    def mergeAlternately(self, word1, word2):
+        """
+        :type word1: str
+        :type  word2: str
+        :rtype: str
+        """
+        m = min(len(word1), len(word2))
+        out = ""
+        i = 0
+        while i < m:
+            out += word1[i] + word2[i]
+            i += 1
+        if i == len(word2):
+            out += word1[i:]
+        else :
+            out += word2[i:]
+        return out
+
+        
+
