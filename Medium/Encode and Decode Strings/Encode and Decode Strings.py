a378f24698196f6b88c6ff7ae94a2c26f17430b8
3zden <azeddineabouaam1@gmail.com>
2026-01-30 15:39:58 +0100

    solve Encode and Decode a String.

---
+++ Medium/Encode and Decode Strings/Encode and Decode Strings.py
@@ -0,0 +1,33 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        if strs == [] :
+            return ""
+        elif strs == [""]:
+            return "\\x"
+        h = {}
+        inn = set()
+        out = []
+            dst = ""
+            for c in st:
+                dst += chr(ord(c)+1)
+            out.append(dst)
+        return " ".join(out)
+                
+
+
+    def decode(self, s: str) -> List[str]:
+        if s == "":
+            return []
+        elif s == "\\x": return [""]
+        h = {}
+        inn = set()
+        out = []
+        spl = s.split(" ")
+        for st in spl:
+            dst = ""
+            for c in st:
+                dst += chr(ord(c)-1)
+            out.append(dst)
+            print(dst) 
+        return out
