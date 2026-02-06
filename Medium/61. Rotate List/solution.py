8dfb131570bcb0a132a0160adf7ce5cb8bfcde30
3zden <azeddineabouaam1@gmail.com>
2026-02-06 10:15:42 +0100

    solve 61. Rotate List

--- Medium/61. Rotate List/solution.py
+++ Medium/61. Rotate List/solution.py
@@ -4,20 +4,24 @@
 #         self.val = val
 #         self.next = next
 class Solution(object):
-    def deleteDuplicates(self, head):
+    def rotateRight(self, head, k):
         """
         :type head: Optional[ListNode]
+        :type k: int
         :rtype: Optional[ListNode]
         """
-        if not head or not head.next : return head
-        stack = [head]
-        prev, curr = head, head.next
+        if k == 0 or not head: return head
+        stack = []
+        curr = head
         while curr:
-            if curr.val != prev.val:
-                prev = curr
-                stack.append(curr)
+            stack.append(curr)
             curr = curr.next
+        n = len(stack)
+        rem = stack[n-k%n:]
+        stack = stack[:n-k%n]
         out = None
         while stack:
             out, out.next = stack.pop(-1), out
+        while rem:
+            out, out.next = rem.pop(-1), out
         return out
